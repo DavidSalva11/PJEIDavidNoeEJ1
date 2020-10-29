1d60edcbc808a5c6fd9fa767c3d99a39e2578706
eskizu <noetorres556@gmail.com>
2020-10-29 13:34:04 +0100

    6B

--- Assets/PracticaGit/PracticaGitBehaviour.cs
+++ Assets/PracticaGit/PracticaGitBehaviour.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 
+
 public class PracticaGitBehaviour : MonoBehaviour {
     [SerializeField]
     [Tooltip("Introducir nombre del autor")]
     private string nombreAutor;
     
     private void Start() {
+        Debug.Log("Autor de la práctica: ");
         Debug.Log(nombreAutor);
     }
 }
